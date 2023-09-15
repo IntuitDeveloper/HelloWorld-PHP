d8bd2d0dfc2a6419104b95ac008c6b8077016bac
Benjamin Laprise <35468343+BenjaminLaprise@users.noreply.github.com>
2023-09-15 14:13:45 -0400

    Fix typo in config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'Enter the clietID from Developer Portal',
+    'client_id' => 'Enter the clientID from Developer Portal',
     'client_secret' => 'Enter the clientSecret from Developer Portal',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
